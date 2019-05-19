df2f0118764350948c464ebe3eda7314ffe2890c
@xouaib <gdasoft18@gmail.com>
2019-05-19 14:41:12 +0000

    add READ file with  logic

---
+++ web_api/read.php
@@ -0,0 +1,13 @@
+<?php
+//Include database config
+include 'config/database.php';
+
+$query = 'SELECT p_id, p_name, p_description, p_price FROM products ORDER BY p_id DESC';
+$stmt = $con->prepare($query);
+$stmt->execute();
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+
+$json = json_encode($results);
+
+echo $json;
