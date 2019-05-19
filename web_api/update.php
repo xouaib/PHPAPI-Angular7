4c77e20583eddb7b9af0caacdcfad502444c7dd5
@xouaib <gdasoft18@gmail.com>
2019-05-19 15:07:35 +0000

    add UPDATE file and update  CREATE FILE

--- web_api/update.php
+++ web_api/update.php
@@ -1,28 +1,30 @@
 <?php
-if ($_POST) {
-    // Include DB Config
-    include 'config/database.php';
-    // Received properties's values
-    const NAME = $_POST['name'];
-    const DESCRIPTION = $_POST['description'];
-    const PRICE = $_POST['price'];
 
+if ($_POST) {
+    include 'config/database.php';
     try {
-        //code...
-        $query = 'INSERT INTO products SET p_name=:name, p_description=:description, p_price=:price';
+        $query = 'UPDATE products 
+                    SET p_name = :name, p_description =: description, p_price = :price 
+                    WHERE p_id = :id';
+
         $stmt = $con->prepare($query);
+
+        const ID = $_POST['id'];
+        const NAME = $_POST['name'];
+        const DESCRIPTION = $_POST['description'];
+        const PRICE = $_POST['price'];
 
         $stmt->bindParam(':name', self::NAME);
         $stmt->bindParam(':description', self::DESCRIPTION);
         $stmt->bindParam(':price', self::PRICE);
+        $stmt->bindParam(':id', self::ID);
 
-        if ($stmt->excute()) {
+        if ($stmt->execute()) {
             echo json_encode(array('result' => 'success'));
         } else {
             echo json_encode(array('result' => 'fail'));
         }
     } catch (PDOException $exception) {
-        //throw $th;
         die('ERROR: ' . $exception->getMessage());
     }
 }
