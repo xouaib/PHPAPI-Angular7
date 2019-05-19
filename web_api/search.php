8cfc43e8b183d68cab9525e691ca778fa25fa416
@xouaib <gdasoft18@gmail.com>
2019-05-19 14:49:45 +0000

    add SEARCH file with logic

---
+++ web_api/search.php
@@ -0,0 +1,20 @@
+<?php
+$id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.'); // raise error if there is param as id with value
+//include database connection
+include 'config/database.php';
+
+try {
+    $query = 'SELECT p_id, p_name, p_description, p_price FROM products WHERE p_id = ? LIMIT 0,1';
+
+    $stmt = $con->prepare($query);
+
+    $stmt->bindParam(1, $id);
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $json = json_encode($row);
+    echo $json;
+} catch (PDOException $exception) {
+    die('ERROR: ' . $exception->getMessage());
+}
