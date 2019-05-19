4b7ec2865f27bedeb04c5c8e356ef3db4c51f6bf
@xouaib <gdasoft18@gmail.com>
2019-05-19 16:05:42 +0000

    #2

--- web_api/create.php
+++ web_api/create.php
@@ -2,19 +2,21 @@
 if ($_POST) {
     include 'config/database.php';
 
-    const NAME = $_POST['name'];
-    const DESCRIPTION = $_POST['description'];
-    const PRICE = $_POST['price'];
-
     try {
-        $query = 'INSERT INTO products SET p_name=:name, p_description=:description, p_price=:price';
+
+        $query = "INSERT INTO products SET p_name=:name, p_description=:description, p_price=:price";
+
         $stmt = $con->prepare($query);
 
-        $stmt->bindParam(':name', NAME);
-        $stmt->bindParam(':description', DESCRIPTION);
-        $stmt->bindParam(':price', PRICE);
+        $name = $_POST['name'];
+        $description = $_POST['description'];
+        $price = $_POST['price'];
 
-        if ($stmt->excute()) {
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':price', $price);
+        
+        if ($stmt->execute()) {
             echo json_encode(array('result' => 'success'));
         } else {
             echo json_encode(array('result' => 'fail'));
